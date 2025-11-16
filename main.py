bf5d24496199f8adee788575d48adf1c5d3e0fec
yedad <yedad@qq.com>
2025-11-16 19:16:31 +0800

    回车

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-
-def log(func):
-    def inner(*args,**kw):
-        print("ahha")
-        return func(*args, **kw)
-    return inner
-
-
-def add(x,y):
-    return x+y
-
-
-
-print(log(add)(1,2))
-
+print("ahha")
